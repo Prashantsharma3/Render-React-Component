af2890a7a2dbd03a5286f24af227f9102d341f78
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-20 22:34:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div id="root">
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+};
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
